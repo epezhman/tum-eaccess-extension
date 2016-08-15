d60cbfe3376a61bd0c501d242c065a61fe7a4017
Pezhman Nasirifard <epezhman@gmail.com>
2016-08-15 12:55:19 +0200

    all

---
+++ background.js
@@ -0,0 +1,24 @@
+chrome.browserAction.onClicked.addListener(function () {
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function (tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        console.assert(typeof url == 'string', 'tab.url should be a string');
+        var pathArray = url.split('/');
+        var protocol = pathArray[0];
+        var host = pathArray[2];
+        var tempUrl = protocol + '//' + host + '.eaccess.ub.tum.de';
+		for(var i = 3; i < pathArray.length; i++)
+		{
+			tempUrl += '/' + pathArray[i];
+		}
+		
+        chrome.tabs.update(tab.id, {url: tempUrl});
+	
+    });
+});
+
