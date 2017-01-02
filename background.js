dca59e0068d81d221424c1c85ba871650fe6eeaa
Pezhman Nasirifard <epezhman@gmail.com>
2017-01-02 11:56:04 +0100

    update to new url format

--- background.js
+++ background.js
@@ -6,16 +6,7 @@ chrome.browserAction.onClicked.addListener(function () {
 
     chrome.tabs.query(queryInfo, function (tabs) {
         var tab = tabs[0];
-        var url = tab.url;
-        console.assert(typeof url == 'string', 'tab.url should be a string');
-        var pathArray = url.split('/');
-        var protocol = pathArray[0];
-        var host = pathArray[2];
-        var tempUrl = protocol + '//' + host + '.eaccess.ub.tum.de';
-	for(var i = 3; i < pathArray.length; i++)
-	{
-		tempUrl += '/' + pathArray[i];
-	}	
-        chrome.tabs.update(tab.id, {url: tempUrl});
+        console.assert(typeof tab.url == 'string', 'tab.url should be a string');
+        chrome.tabs.update(tab.id, {url: 'https://eaccess.ub.tum.de/login?url=' + tab.url});
     });
 });
